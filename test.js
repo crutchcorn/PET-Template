a01ed524b1b7b52c032160426837ac845758e58b
Crutchcorn <crutchcorn@gmail.com>
2018-03-24 17:26:35 -0700

    Don't you love rewriting the hell out of your own code?

--- test.js
+++ test.js
@@ -1,32 +1,40 @@
+/**
+ * THIS IS GOING TO BE A MASSIVE LIST
+ * TODO: Make sure that key order does not affect how things work. You should be able to do `['client', 'options']` and
+ * have it work just as well as `['options', 'client']`
+ * TODO: Make sure that requires does not apply when the path is not completed going up to it
+ * (if net deps on opt and is required, but opt is not selected (and is not required), dont query for it)
+ * TODO: If something is required and selected and has required children, it should query for all of them
+ * TODO: Make sure all required items at root are found
+ */
+
+const keys = ['client', 'network'];
+
 const script = [
 	{
+		name: 'name',
+		required: true
+	},
+	{
 		name: 'client',
-		depends: []
+		required: (keys) => !keys.includes('server')
 	}, {
 		name: 'server',
-		depends: []
+		required: (keys) => !keys.includes('client')
 	}, {
 		name: 'options',
-		depends: [['client'], ['server']]
+		depends: [['client'], ['server']],
+		required: true
 	}, {
 		name: 'network',
 		depends: ['options']
 	}, {
 		name: 'ajax',
-		depends: ['client', 'options', 'network']
+		depends: ['client', 'options', 'network'],
+		required: true
 	}
-];
+].map(item => ({...item, depends: item.depends ? item.depends : [], required: typeof item.required === 'function' ? item.required(keys) : !!item.required}));
 
-/* {
-	client: [],
-	server: [],
-	options: [['client'], ['server']],
-	network: ['options'],
-    ajax: ['client', 'options', 'network']
-}; */
-const obj = script.reduce((prev, x) => {
-	return {...prev, ...{[x.name]: x.depends}}
-}, {});
 
 /* {
 	client: [[]],
@@ -35,11 +43,10 @@ const obj = script.reduce((prev, x) => {
 	network: [['client', 'options'], ['server', 'options']],
     ajax: [['client', 'options', 'network']]
 }; */
-const findObjectChildren = (obj, seen = []) => Object.keys(obj).reduce((prev, key) => {
+const cleanObject = (obj, seen = []) => Object.keys(obj).reduce((prev, key) => {
 	if (!Array.isArray(obj[key])) {
 		throw new Error(key + 'is not an array. All deps must be an array')
 	}
-	;
 	if (obj[key].length > 0) {
 		if (!obj[obj[key][0]]) {
 			throw new Error(key + '-' + obj[key][0] + ' does not refer to a valid path')
@@ -48,7 +55,6 @@ const findObjectChildren = (obj, seen = []) => Object.keys(obj).reduce((prev, ke
 		if (!Array.isArray(refArr)) {
 			throw new Error(obj[key][0] + 'is not an array. All deps must be an array')
 		}
-		;
 		if (refArr.length > 0) {
 			// All items in array must be array
 			if (Array.isArray(refArr[0])) {
@@ -58,7 +64,6 @@ const findObjectChildren = (obj, seen = []) => Object.keys(obj).reduce((prev, ke
 							if (!Array.isArray(items)) {
 								throw new Error(obj[key][0] + ' must not have a combination of arrays and non-arrays')
 							}
-							;
 							seen.push(items[0]);
 							const circDep = seen.find(see => see === key)
 							if (!!circDep) {
@@ -74,7 +79,6 @@ const findObjectChildren = (obj, seen = []) => Object.keys(obj).reduce((prev, ke
 					if (Array.isArray(item)) {
 						throw new Error(obj[key][0] + ' must not have a combination of arrays and non-arrays')
 					}
-					;
 				});
 				seen.push(refArr[0]);
 				const circDep = seen.find(see => see === key)
@@ -83,16 +87,28 @@ const findObjectChildren = (obj, seen = []) => Object.keys(obj).reduce((prev, ke
 				}
 
 				// Placing into double array for simpler
-				return {...prev, ...{[key]: [[...obj[key], ...refArr]]}};
+				return { ...prev, ...{ [key]: [[...obj[key], ...refArr]] } };
 			}
+			// Array is empty
 		} else {
-			return {...prev, ...{[key]: [obj[key]]}};
+			return { ...prev, ...{ [key]: obj[key] } };
 		}
+		// Array is empty
 	} else {
-		return {...prev, ...{[key]: [obj[key]]}};
+		return { ...prev, ...{ [key]: [obj[key]] } };
 	}
 }, {});
 
+/* {
+	client: [],
+	server: [],
+	options: [['client'], ['server']],
+	network: ['options'],
+    ajax: ['client', 'options', 'network']
+}; */
+const obj = cleanObject(script.reduce((prev, x) => {
+	return { ...prev, ...{ [x.name]: x.depends ? x.depends : [] } }
+}, {}));
 
 /*
 obj = {
@@ -102,32 +118,73 @@ obj = {
     network: [["options","client"], ["options","server"]],
     ajax: [["client","options","network"]]
 }
-keys = ['client', 'network', 'options']
 */
-const matchKeys = (obj, keys) => {
-	/*[
-		{ "client": [false] },
-		{ "server": [false] },
-		{ "options": [false, false] },
-		{ "network": [true, false] },
-		{ "ajax": [false] }
-	]*/
+const getChildren = (obj, keys) => {
+	/*{
+		"client": [false],
+		"server": [false],
+		"options": [false, false],
+		"network": [true, false],
+		"ajax": [false]
+	}*/
 	// Defaults to finding exact matches
-	const findMap = (arrComp = 'every', compareFn = ((arrLen, subArrLen) => arrLen === subArrLen)) =>
-		Object.keys(obj).map(key => ({
-			[key]: obj[key].map(subArr => compareFn(keys.length, subArr.length) ?
+	// Does not suport arr to be [], must be fixed
+	// TODO: Change this from an array to an object with keys dammit Corbin
+	const findMap = (arr = keys, arrComp = 'every', compareFn = ((arrLen, subArrLen) => arrLen === subArrLen)) =>
+		Object.keys(obj).reduce((prev, key) => ({...prev,
+			[key]: obj[key].map(subArr => compareFn(arr.length, subArr.length) ?
 				// This could be rewritten as subArr[arrComp] but was not for code clarity and safety
-				Array.prototype[arrComp].bind(subArr)(item => keys.find(find => find === item)) :
+				Array.prototype[arrComp].bind(subArr)(item => arr.find(find => find === item)) :
 				false)
-		}));
+		}), {});
+
+	// THIS IS VERY UNPERFORMANT - PLEASE SIMPLY ADD THIS FUNCTIONALITY TO findMap
+	// ['client']
+	// THIS MIGHT WORK NOW
+	const flattenFindMap = (mapRes) => Object.keys(mapRes).reduce((prev, key) => mapRes[key].find(bool => !!bool) ? [...prev, key] : prev, []);
+
+
+
+	// THIS IS VERY UNPERFORMANT - PLEASE SIMPLY ADD THIS FUNCTIONALITY TO findMap
+	// [{key: 'client': index: 3}]
+	const indexFindMap = (mapRes) => Object.keys(mapRes).reduce((prev, mapKey) => {
+		// This can be a findIndex because there should be no more than a single instance that matches exactly the same in a key. If we wanted to, we could use reduce and add an error message if this did exist
+		keyIndex = mapRes[mapKey].findIndex(bool => !!bool);
+		return keyIndex !== -1 ? [...prev, { key: mapKey, index: keyIndex }] : prev
+	}, []);
+
+	// Start at root, find matching deps the rely on that, if required, then find deps that rely on that, and that and that
+	// for each key, find matching deps the rely on that, if required, then find deps that rely on that, and that and that
+
+	// This should work?
+	const findPaths = (arr = keys) => {
+		const tmpMap = findMap(arr);
+		// [{key: 'client': index: 3}]
+		objKeys = indexFindMap(tmpMap);
+		console.log(objKeys);
+		// Find index at which the findMap was true to pass to findPaths
+		const requiredKeys = objKeys.filter(objKey => script.find(scriptItem => scriptItem.name === objKey.key).required);
+		// Find required paths of children of required paths
+		return requiredKeys.reduce((prev, reqKey) => [...prev, reqKey.key, ...findPaths([...obj[reqKey.key][reqKey.index], reqKey.key], true)], []);
+	};
+
+	const findKeysPaths = () => {
+		keys.reduce((prev, key, index, array) => {
+			const pathToFind = array.slice(0, index + 1);
+			return [...prev, ...findPaths(pathToFind)];
+		}, [])
+	};
 
 
 	matchMap = findMap();
-	matchChildrenMap = findMap('every', (arrLen, subArrLen) => arrLen < subArrLen);
+	matchChildrenMap = findMap(keys, 'every', (arrLen, subArrLen) => arrLen < subArrLen);
 	// To get `proximity` distance from, we need to make the `true` and `false` to return how many were accepted and how many weren't
 	// closeMatchChildren = findMap('some', (arrLen, subArrLen) => true);
 
+	// Get everything in the same branches of the graph
+
 	// 'matchKey'
+	// THIS MIGHT WORK NOW
 	match = Object.keys(matchMap).reduce((prev, key) => {
 		if (matchMap[key].find(bool => !!bool)) {
 			if (!!prev) {
@@ -142,5 +199,11 @@ const matchKeys = (obj, keys) => {
 	}, '');
 
 	// ['key', 'child']
-	matchChildren = Object.keys(matchChildrenMap).reduce((prev, key) => matchChildrenMap[key].find(bool => !!bool) ? [...prev, key] : prev, []);
+	matchChildren = flattenFindMap(matchChildrenMap);
+	return matchChildren;
 };
+
+// children = ['options', 'test']
+// const checkIfRequired = (obj, children, keys) => {
+// 	children
+// }
