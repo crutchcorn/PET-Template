eda982181823475cb9b493d0a0871f729407d2b7
Crutchcorn <crutchcorn@gmail.com>
2018-05-07 18:58:55 -0700

    Server: Fix spacing and database info for initial load screen

--- templates/servers/PET/src/config/lib/app.ts
+++ templates/servers/PET/src/config/lib/app.ts
@@ -6,20 +6,29 @@ const config: configReturn = require('../config');
 import {init as expressInit} from './express';
 import {Express as appType} from "express-serve-static-core";
 import chalk from 'chalk';
-import {createConnection} from 'typeorm';
+import {Connection, createConnection} from 'typeorm';
 
-export function init(callback: (app: appType, config: configReturn) => void) {
+const spaces = 5;
+const maxSize = Math.max(...[
+  'Environment',
+  'Server',
+  'App version',
+  'Database',
+  '{{titleCase name}} version'
+].map(text => text.length)) + 1;
+
+export function init(callback: (app: appType, config: configReturn, connection: Connection) => void) {
   createConnection()
     .then(async connection => {
       // Initialize express
       var app: appType = expressInit();
-      if (callback) callback(app, config);
+      if (callback) callback(app, config, connection);
     })
     .catch(error => console.log(chalk.red('TypeORM connection error: ', error)));
 }
 
 export function start(callback?: (app: appType, config: configReturn) => any) {
-  init(function (app: appType, config: configReturn) {
+  init(function (app: appType, config: configReturn, connection: Connection) {
 
     // Start the app by listening on <port> at <host>
     app.listen(<number>config.port, config.host, function () {
@@ -29,15 +38,26 @@ export function start(callback?: (app: appType, config: configReturn) => any) {
       console.log('--');
       console.log(chalk.green(config.app.title));
       console.log();
-      console.log(chalk.green('Environment:     ' + process.env.NODE_ENV));
-      console.log(chalk.green('Server:          ' + server));
-      // TODO: Add database information to info here
-      // TODO: Fix the styling of spacing
-      // console.log(chalk.green('Database:        ' + config.db.uri));
-      console.log(chalk.green('App version:     ' + config.{{camelCase name}}.version));
-      // TODO: Add info to {{dashCase name}}-version
+      console.log(chalk.green('Environment:' + ' '.repeat((maxSize - 'Environment'.length) + spaces) + process.env.NODE_ENV));
+      console.log(chalk.green('Server:'  + ' '.repeat((maxSize - 'Server'.length) + spaces) + server));
+      const optsDB = connection.options.database;
+      const optsURL = (<{url: string}>connection.options).url;
+      const optsLoc = (<{location: string}>connection.options).location;
+      let location: string = '';
+      if (optsURL) {
+        // MySQL
+        location = optsURL;
+      } else if (optsDB && !Array.isArray(optsDB)) {
+        // SQLite
+        location = <string>connection.options.database;
+      } else if (optsLoc) {
+        // SQLjs
+        location = optsLoc;
+      }
+      console.log(chalk.green('Database:' + ' '.repeat((maxSize - 'Database'.length) + spaces) + location));
+      console.log(chalk.green('App version:'  + ' '.repeat((maxSize - 'App version'.length) + spaces) + config.{{camelCase name}}.version));
       if (config.{{camelCase name}}['{{dashCase name}}-version']) {
-        console.log(chalk.green('{{titleCase name}} version:     ' + config.{{camelCase name}}['{{dashCase name}}-version']));
+        console.log(chalk.green('{{titleCase name}} version:'  + ' '.repeat((maxSize - '{{titleCase name}} version'.length) + spaces) +  config.{{camelCase name}}['{{dashCase name}}-version']));
       }
       console.log('--');
 
